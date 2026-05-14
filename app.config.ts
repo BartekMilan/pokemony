475a63e8492c453c13daa6fd59b54ad6b4a0aed1
Bartłomiej Milan <bartlomiej.milan@swmansion.com>
2026-05-14 14:15:38 +0200

    fix: Test free cursor & merge app.json and app.config.ts to one file

--- app.config.ts
+++ app.config.ts
@@ -1,19 +1,77 @@
 import type { ConfigContext, ExpoConfig } from 'expo/config';
 
+const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY ?? '';
+
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  android: {
-    ...config.android,
-    config: {
-      googleMaps: {
-        apiKey: process.env.GOOGLE_MAPS_API_KEY ?? '',
-      },
-    },
+  name: 'temp-app',
+  slug: 'temp-app',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  userInterfaceStyle: 'light',
+  newArchEnabled: true,
+  splash: {
+    image: './assets/splash-icon.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
   },
   ios: {
     ...config.ios,
+    supportsTablet: true,
+    bundleIdentifier: 'com.anonymous.moj-pokedex-app',
     config: {
-      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY ?? '',
+      ...config.ios?.config,
+      googleMapsApiKey: GOOGLE_MAPS_API_KEY,
     },
   },
+  android: {
+    ...config.android,
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#ffffff',
+    },
+    edgeToEdgeEnabled: true,
+    predictiveBackGestureEnabled: false,
+    package: 'com.anonymous.tempapp',
+    config: {
+      ...config.android?.config,
+      googleMaps: {
+        apiKey: GOOGLE_MAPS_API_KEY,
+      },
+    },
+  },
+  web: {
+    favicon: './assets/favicon.png',
+  },
+  plugins: [
+    [
+      'react-native-vision-camera',
+      {
+        cameraPermissionText:
+          'Allow temp-app to use your camera to overlay your favorite Pokémon on photos.',
+        enableFrameProcessors: true,
+      },
+    ],
+    [
+      'expo-location',
+      {
+        locationWhenInUsePermission:
+          'Allow $(PRODUCT_NAME) to use your location to show Pokémon near you.',
+        locationAlwaysAndWhenInUsePermission:
+          'Allow $(PRODUCT_NAME) to use your location to show Pokémon near you.',
+      },
+    ],
+    [
+      'expo-build-properties',
+      {
+        ios: {
+          deploymentTarget: '16.0',
+        },
+        android: {
+          minSdkVersion: 26,
+        },
+      },
+    ],
+  ],
 });
